673e052df9226b62635052cb868eb0f16828d0ea
Jon Bright <jon@siliconcircus.com>
2022-01-25 19:59:16 +0000

    Cleanup mathtest.s : use symbolic names for A, B, R. Switch to asm-mode indentation for comments. Remove trailing tabs.

--- mathtest.s
+++ mathtest.s
@@ -2,9 +2,9 @@ RES = $3000
 
 #include "call.s"
 
-* = $8000      ; $8000 is the bottom of our ROM
+* = $8000 			; $8000 is the bottom of our ROM
 reset:
-	ldx #$ff       ; Point at top of stack
+	ldx #$ff		; Point at top of stack
 	txs
 
 	jsr lcd_init
@@ -13,73 +13,73 @@ reset:
 test1:
 	lda #'1'
 	jsr lcd_char_out
-	
+
 	lda #(num1 & $ff)
-	sta $00
+	sta A
 	lda #(num1 >> 8)
-	sta $01
+	sta A_
 
 	lda #(num2 & $ff)
-	sta $02
+	sta B
 	lda #(num2 >> 8)
-	sta $03
+	sta B_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
+	sta R_
 
 	ldy #8
-	
+
 	jsr add
 
 	lda #(res_1_2_add & $ff)
-	sta $00
+	sta A
 	lda #(res_1_2_add >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test2
 	jmp err_num_out
-	
+
 	;; Test 2, add num1 and num3, compare
 test2:
 	lda #'2'
 	jsr lcd_char_out
 
 	lda #(num1 & $ff)
-	sta $00
+	sta A
 	lda #(num1 >> 8)
-	sta $01
+	sta A_
 
 	lda #(num3 & $ff)
-	sta $02
+	sta B
 	lda #(num3 >> 8)
-	sta $03
+	sta B_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr add
 
 	lda #(res_1_3_add & $ff)
-	sta $00
+	sta A
 	lda #(res_1_3_add >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test3
@@ -91,33 +91,33 @@ test3:
 	jsr lcd_char_out
 
 	lda #(num4 & $ff)
-	sta $00
+	sta A
 	lda #(num4 >> 8)
-	sta $01
+	sta A_
 
 	lda #(num5 & $ff)
-	sta $02
+	sta B
 	lda #(num5 >> 8)
-	sta $03
+	sta B_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr add
 
 	lda #(res_4_5_add & $ff)
-	sta $00
+	sta A
 	lda #(res_4_5_add >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test4
@@ -129,28 +129,28 @@ test4:
 	jsr lcd_char_out
 
 	lda #(num1 & $ff)
-	sta $00
+	sta A
 	lda #(num1 >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr rsh
 
 	lda #(res_num1_rsh & $ff)
-	sta $00
+	sta A
 	lda #(res_num1_rsh >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test5
@@ -162,28 +162,28 @@ test5:
 	jsr lcd_char_out
 
 	lda #(num5 & $ff)
-	sta $00
+	sta A
 	lda #(num5 >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr rsh
 
 	lda #(res_num5_rsh & $ff)
-	sta $00
+	sta A
 	lda #(res_num5_rsh >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test6
@@ -195,28 +195,28 @@ test6:
 	jsr lcd_char_out
 
 	lda #(num1 & $ff)
-	sta $00
+	sta A
 	lda #(num1 >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr lsh
 
 	lda #(res_num1_lsh & $ff)
-	sta $00
+	sta A
 	lda #(res_num1_lsh >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test7
@@ -228,28 +228,28 @@ test7:
 	jsr lcd_char_out
 
 	lda #(num5 & $ff)
-	sta $00
+	sta A
 	lda #(num5 >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr lsh
 
 	lda #(res_num5_lsh & $ff)
-	sta $00
+	sta A
 	lda #(res_num5_lsh >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test8
@@ -259,35 +259,35 @@ test7:
 test8:
 	lda #'8'
 	jsr lcd_char_out
-	
+
 	lda #(num1 & $ff)
-	sta $00
+	sta A
 	lda #(num1 >> 8)
-	sta $01
+	sta A_
 
 	lda #(num2 & $ff)
-	sta $02
+	sta B
 	lda #(num2 >> 8)
-	sta $03
+	sta B_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
-	
+	sta R_
+
 	ldy #8
-	
+
 	jsr mul
 
 	lda #(res_1_2_mul & $ff)
-	sta $00
+	sta A
 	lda #(res_1_2_mul >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc test9
@@ -300,33 +300,33 @@ test9:
 	jsr lcd_char_out
 
 	lda #(num2 & $ff)
-	sta $00
+	sta A
 	lda #(num2 >> 8)
-	sta $01
+	sta A_
 
 	lda #(num1 & $ff)
-	sta $02
+	sta B
 	lda #(num1 >> 8)
-	sta $03
+	sta B_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
+	sta R_
 
 	ldy #8
-	
+
 	jsr mul
 
 	lda #(res_1_2_mul & $ff)
-	sta $00
+	sta A
 	lda #(res_1_2_mul >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc testA
@@ -336,35 +336,35 @@ test9:
 testA:
 	lda #'A'
 	jsr lcd_char_out
-	
+
 	lda #(num6 & $ff)
-	sta $00
+	sta A
 	lda #(num6 >> 8)
-	sta $01
+	sta A_
 
 	lda #(num7 & $ff)
-	sta $02
+	sta B
 	lda #(num7 >> 8)
-	sta $03
+	sta B_
 
 	lda #(RES & $ff)
-	sta $04
+	sta R
 	lda #(RES >> 8)
-	sta $05
+	sta R_
 
 	ldy #4
-	
+
 	jsr mul
 
 	lda #(res_6_7_mul & $ff)
-	sta $00
+	sta A
 	lda #(res_6_7_mul >> 8)
-	sta $01
+	sta A_
 
 	lda #(RES & $ff)
-	sta $02
+	sta B
 	lda #(RES >> 8)
-	sta $03
+	sta B_
 
 	jsr cmpeq
 	bcc testB
@@ -521,7 +521,7 @@ msg_ok:
 	.asc "All tests pass!",0
 
 num1:
-; 12345678901 decimal as a little-endian 64-bit number
+	;; 12345678901 decimal as a little-endian 64-bit number
 	.byte $35
 	.byte $1c
 	.byte $dc
@@ -533,7 +533,7 @@ num1:
 
 
 num2:
-; 3 decimal as a little-endian 64-bit number
+	;; 3 decimal as a little-endian 64-bit number
 	.byte $03
 	.byte $00
 	.byte $00
@@ -544,7 +544,7 @@ num2:
 	.byte $00
 
 num3:
-; A number big enough to cause a few overflows on addition with num1
+	;; A number big enough to cause a few overflows on addition with num1
 	.byte $d2
 	.byte $34
 	.byte $44
@@ -553,9 +553,9 @@ num3:
 	.byte $00
 	.byte $01
 	.byte $01
-	
+
 num4:
-; Pattern number
+	;; Pattern number
 	.byte $55
 	.byte $aa
 	.byte $55
@@ -566,7 +566,7 @@ num4:
 	.byte $aa
 
 num5:
-; Pattern number
+	;; Pattern number
 	.byte $aa
 	.byte $55
 	.byte $aa
@@ -577,28 +577,28 @@ num5:
 	.byte $55
 
 num6:
-; 32-bit ~sqrt of FFFFFFFF
+	;; 32-bit ~sqrt of FFFFFFFF
 	.byte $ff
 	.byte $ff
 	.byte $00
 	.byte $00
 
 num7:
-; 32-bit ~sqrt of FFFFFFFF (other half)
+	;; 32-bit ~sqrt of FFFFFFFF (other half)
 	.byte $01
 	.byte $00
 	.byte $01
 	.byte $00
 
 num8:
-; 1 more than num7, for purpose of subtraction test
+	;; 1 more than num7, for purpose of subtraction test
 	.byte $02
 	.byte $00
 	.byte $01
 	.byte $00
-	
+
 res_1_2_mul:
-; 37037036703 decimal as a little-endian 64-bit number. Result of multiplying num1 and num2.
+	;; 37037036703 decimal as a little-endian 64-bit number. Result of multiplying num1 and num2.
 	.byte $9f
 	.byte $54
 	.byte $94
@@ -609,7 +609,7 @@ res_1_2_mul:
 	.byte $00
 
 res_1_2_add:
-; 12345678904 decimal as a little-endian 64-bit number. Result of adding num1 and num2.
+	;; 12345678904 decimal as a little-endian 64-bit number. Result of adding num1 and num2.
 	.byte $38
 	.byte $1c
 	.byte $dc
@@ -620,7 +620,7 @@ res_1_2_add:
 	.byte $00
 
 res_1_3_add:
-; Result of adding num1 and num3.
+	;; Result of adding num1 and num3.
 	.byte $07
 	.byte $51
 	.byte $20
@@ -629,9 +629,9 @@ res_1_3_add:
 	.byte $01
 	.byte $01
 	.byte $01
-	
+
 res_4_5_add:
-; Result of adding num4 and num5.
+	;; Result of adding num4 and num5.
 	.byte $ff
 	.byte $ff
 	.byte $ff
@@ -642,7 +642,7 @@ res_4_5_add:
 	.byte $ff
 
 res_num1_rsh:
-; num1 shifted right by one bit
+	;; num1 shifted right by one bit
 	.byte $1a
 	.byte $0e
 	.byte $ee
@@ -653,7 +653,7 @@ res_num1_rsh:
 	.byte $00
 
 res_num5_rsh:
-; num5 shifted right by one bit
+	;; num5 shifted right by one bit
 	.byte $d5
 	.byte $2a
 	.byte $d5
@@ -664,7 +664,7 @@ res_num5_rsh:
 	.byte $2a
 
 res_num1_lsh:
-; num1 shifted left by one bit
+	;; num1 shifted left by one bit
 	.byte $6a
 	.byte $38
 	.byte $b8
@@ -675,7 +675,7 @@ res_num1_lsh:
 	.byte $00
 
 res_num5_lsh:
-; num5 shifted right by one bit
+	;; num5 shifted right by one bit
 	.byte $54
 	.byte $ab
 	.byte $54
@@ -686,14 +686,14 @@ res_num5_lsh:
 	.byte $ab
 
 res_6_7_mul:
-; num6 and num7 multiplied
+	;; num6 and num7 multiplied
 	.byte $ff
 	.byte $ff
 	.byte $ff
 	.byte $ff
 
 res_3_1_sub:
-; num3 minus num1
+	;; num3 minus num1
 	.byte $9d
 	.byte $18
 	.byte $68
@@ -704,27 +704,28 @@ res_3_1_sub:
 	.byte $01
 
 res_7_7_sub:
-; num7 minus num7 - zero
+	;; num7 minus num7 - zero
 	.byte $00
 	.byte $00
 	.byte $00
 	.byte $00
 
 res_7_8_sub:
-; num7 minus num8 - minus 1
+	;; num7 minus num8 - minus 1
 	.byte $ff
 	.byte $ff
 	.byte $ff
 	.byte $ff
-	
-; The section below will pad out the file to 32k and also insert the interrupt and reset vectors.
-; The reset vector will point at address $8000, which is where the ROM is mapped to for the 6502
-; (and what we set as start address above).
-codeend:
-* = $fffa
-.dsb (*-codeend), $ea    ; Fill everything from end of code to vectors with NOPs
-* = $fffa
 
-.word $0000  ; NMI Vector
-.word reset  ; Reset Vector
-.word $0000  ; BRK/IRQB Vector
+
+codeend:
+	;; This section will pad out the file to 32k and also insert the interrupt and reset vectors.
+	;; The reset vector will point at address $8000, which is where the ROM is mapped to for our
+	;; 6502 (and what we set as start address above).
+
+* = $fffa
+	.dsb (*-codeend), $ea	; Fill everything from end of code to vectors with NOPs
+* = $fffa
+	.word $0000		; NMI Vector
+	.word reset		; Reset Vector
+	.word $0000		; BRK/IRQB Vector
